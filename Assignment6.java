38e25905d857f9d71489d1e3e0419416decdd96e
S <s>
2024-01-01 21:36:03 +0400

    {"types":"A","runs":33}

---
+++ Assignment6.java
@@ -0,0 +1,22 @@
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+
+public class Assignment6 extends GraphicsProgram{
+
+	public void run() {
+		double size = 50;
+		double x = (getWidth()-size)/2;
+		double y = (getHeight()-size)/2;
+		add(drawRect(size,Color.green,true),x,y);
+		
+	}
+	
+	private GRect drawRect(double size, Color color, boolean flag) {
+		GRect rect = new GRect(size,size);
+		rect.setFilled(flag);
+		rect.setColor(color);
+		return rect;
+	}
+}
