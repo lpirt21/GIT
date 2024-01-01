0863a8d5b98a2a639bd21ab8c0c5e42ddfac178e
S <s>
2024-01-01 21:43:41 +0400

    {"types":"A","runs":36}

---
+++ Assignment7.java
@@ -0,0 +1,34 @@
+import acm.program.*;
+
+import java.awt.Color;
+
+import acm.graphics.*;
+
+
+public class Assignment7 extends ConsoleProgram{
+
+	public void run() {
+		for(int i =1; i<6; i++) {
+			int size = fibonacci(i)*10;
+			double x = size+ 10*i;
+			add(drawRect(size),x,0);
+		}
+	}
+	
+	private GRect drawRect(double size) {
+		GRect rect = new GRect(size,size);
+		return rect;
+	}
+	
+	private int fibonacci(int n) {
+		int f1 = 0;
+		int f2 = 1;
+		int fi = 0;
+		for(int i = 0; i < n; i++) {
+			fi = f1 + f2;
+			f1 = f2;
+			f2 = fi;
+		}
+		return f1;
+	}
+}
