14fc0e9d1e3f056ff3d97fe630bb52243448f8c9
S <s>
2024-01-01 22:34:26 +0400

    {"types":"C","runs":84}

--- Assignment8.java
+++ Assignment8.java
@@ -1,4 +1,21 @@
+import acm.program.*;
 
-public class Assignment8 {
 
+public class Assignment8 extends ConsoleProgram{
+
+	public void run() {
+		int n1 = readInt("Enter number 1: ");
+		int n2 = readInt("Enter number 2:");
+		println("gcd of " + n1 + " and " + n2 + " is " + euclidianGCD(n1,n2));
+	}
+	
+	private int euclidianGCD(int x, int y) {
+		int r = x%y;
+		while(r!=0) {
+			x = y;
+			y = r;
+			r = x%y;
+		}
+		return y;
+	}
 }
