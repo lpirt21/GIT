daefca3e3bcff41b1a8b4cb38dfdd13d79ba6c25
S <s>
2024-01-01 21:59:59 +0400

    {"types":"C","runs":47}

--- Assignment7.java
+++ Assignment7.java
@@ -23,13 +23,13 @@ public class Assignment7 extends GraphicsProgram{
 		size = fibonacci(3)*10;
 		add(drawRect(size),x,y);
 		x += size;
+		y+=size;
+		size = fibonacci(4)*10;
+		add(drawRect(size),x,y);
 		size = fibonacci(2)*10;
 		add(drawRect(size),x,y);
 		y+=size;
 		size = fibonacci(1)*10;
-		add(drawRect(size),x,y);
-		y+=size;
-		size = fibonacci(4)*10;
 		add(drawRect(size),x,y);
 	}
 	
