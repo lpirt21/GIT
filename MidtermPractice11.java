993fc820c84abb8ffec0289937a3d4362ce53c96
S <s>
2024-01-02 18:35:30 +0400

    {"types":"C","runs":100}

--- MidtermPractice11.java
+++ MidtermPractice11.java
@@ -13,7 +13,7 @@ public class MidtermPractice11 extends ConsoleProgram{
 		println(reversedBinary(n));
 		println(binary(n));
 		println(reverseTheBinary(n));
-		println(2*binaryToDecimal(n));
+		println(binaryToDecimal(n));
 	}
 	
 	private String reversedBinary(int n) {
