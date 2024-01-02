540804d5b6bada177696333a98fd514ec09ae85c
S <s>
2024-01-02 18:52:20 +0400

    {"types":"A","runs":114}

---
+++ MidtermPractice12.java
@@ -0,0 +1,46 @@
+
+
+import acm.program.*;
+import acm.util.RandomGenerator;
+
+public class MidtermPractice12 extends ConsoleProgram{
+
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+		int n = readInt("Enter a number: ");
+		int k = readInt("Enter a base: ");
+		println(n+ " wtih the base " + k + " is " +changeTheBase(n,k));
+		println("The sum of all numbers of " + n+ " wtih the base " + k + " is " +theSum(n,k));
+	}
+	
+	
+	private int theSum(int n, int k) {
+		int dec=0;
+		int b=0;
+		while(n>0) {
+			b = n%k;
+			dec+=b;
+			n/=k;
+		}
+		
+		return dec;
+	}
+	
+
+	private String changeTheBase(int n, int k) {
+		String dec="";
+		String res = "";
+		int b=0;
+		while(n>0) {
+			b = n%k;
+			dec+=""+b;
+			n/=k;
+		}
+		for(int i= dec.length()-1; i>=0; i++) {
+			res+=dec.charAt(i);
+		}
+		return res;
+	}
+}
+
