2e1949284cf841062065991720db284caddc3732
S <s>
2024-01-02 18:35:11 +0400

    {"types":"C","runs":98}

--- MidtermPractice11.java
+++ MidtermPractice11.java
@@ -61,7 +61,7 @@ public class MidtermPractice11 extends ConsoleProgram{
 		int b =0;
 		int two = 1;
 		int decimal = 0;
-		for(int j =binary1.length()-1; j>=0; j--) {
+		for(int j =binary1.length()-1; j>0; j--) {
 			char ch = binary1.charAt(j);
 			String number = ""+ch;
 			b = Integer.parseInt(number);
