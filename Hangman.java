39fad6b83dfbd3926d3894666e504c81d9b53985
S <s>
2024-01-06 03:13:43 +0400

    {"types":"C","runs":4}

--- Hangman.java
+++ Hangman.java
@@ -11,9 +11,9 @@ import acm.program.*;
 import acm.util.*;
 
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javax.swing.JFileChooser;
 
@@ -22,18 +22,62 @@ public class Hangman extends ConsoleProgram {
 	
 	RandomGenerator rgen = RandomGenerator.getInstance();
 	private final String fileName = "ShorterLexicon.txt";
+	private ArrayList <String> theWords;
 
 	
 	public void run(){
-		try {
-			PrintWriter wr = new PrintWriter(new FileWriter("Hello.txt"));
-			wr.println("Hello world");
-			wr.close();
-		}catch(IOException ex) {
-			throw new ErrorException(ex);
+		String name = fileName;
+		ArrayList<String> lines  = openFileReader(name);
+		transform(lines);
+		for(int i = lines.size()-1; i>=0; i--) {
+			println(lines.get(i));
 		}
 	}
 	
+	
+	private void transform(ArrayList<String> lines) {
+		String sentence = "";
+		int small = 'a';
+		int small2 = 'z';
+		int big = 'A';
+		int big2 = 'Z';
+		String token2= "";
+		for(int i =0; i<lines.size(); i++) {
+		String line1 = lines.get(i);
+		StringTokenizer tokenizer = new StringTokenizer(line1, " .,!?:;\"'");
+		while(tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			for(int j =0; j<token.length(); j++) {
+				if(Character.isLowerCase(token.charAt(j))) {
+					char ch2 = (char)rgen.nextInt(small,small2);
+					token2+=ch2;
+				}else if(Character.isUpperCase(token.charAt(j))) {
+					char ch2 = (char)rgen.nextInt(big,big2);
+					token2+=ch2;
+				}else {
+					token2+=token;
+				}
+			}
+			sentence+=token2;
+		}
+		}
+	}
+	//reads each line and adds each read word in an arraylist.
+	private ArrayList<String> openFileReader(String fileName) {
+		theWords = new ArrayList<String>();
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader(fileName));
+			while(true) {
+				String line = rd.readLine();
+				if(line == null)break;
+				theWords.add(line);
+			}
+			rd.close();
+		}catch(IOException ex) {
+			println("can't open that file");
+		}
+		return theWords;
+	}
 
 }
 
