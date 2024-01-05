da5158cfdf54e1e7f37d8816f6c2a76c748dad23
S <s>
2024-01-06 02:55:11 +0400

    {"types":"C","runs":2}

--- Hangman.java
+++ Hangman.java
@@ -12,12 +12,8 @@ import acm.util.*;
 
 import java.awt.*;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 import javax.swing.JFileChooser;
 
@@ -27,40 +23,15 @@ public class Hangman extends ConsoleProgram {
 	RandomGenerator rgen = RandomGenerator.getInstance();
 	private final String fileName = "ShorterLexicon.txt";
 
-
 	
 	public void run(){
-		println("This program reverses the lines in a file");
-		BufferedReader reader = openFile();
-		readFile(reader);
-	}
-	
-	
-	private void readFile(BufferedReader rd) {
 		try {
-			while(true) {
-			String line = rd.readLine();
-			if(line == null) break;
-			println(line);
-			}
+			PrintWriter wr = new PrintWriter(new FileWriter("Hello.txt"));
+			wr.println("Hello world");
+			wr.close();
 		}catch(IOException ex) {
-			println("cant");
+			throw new ErrorException(ex);
 		}
-	}
-	
-	private BufferedReader openFile() {
-		BufferedReader rd = null;
-		JFileChooser chooser = new JFileChooser();
-		int result = chooser.showOpenDialog(this);
-		if(result == JFileChooser.APPROVE_OPTION) {
-			try {
-				File file = chooser.getSelectedFile();
-				rd = new BufferedReader(new FileReader(file));
-			}catch(IOException ex) {
-				println("Can't open that file");
-			}
-		}
-		return rd;
 	}
 	
 
