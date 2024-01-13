62cedd4437d7844e2c60010fde621dff7181f8a6
S <s>
2024-01-13 13:31:41 +0400

    {"types":"C","runs":9}

--- Ch5Ex6.java
+++ Ch5Ex6.java
@@ -4,30 +4,31 @@ import acm.program.*;
 import java.awt.*;
 import acm.graphics.*;
 
-public class Ch5Ex6 extends GraphicsProgram{
+public class Ch5Ex6  extends GraphicsProgram{
 	public void run() {
-	    double x = getWidth()/2;
-	    double y = getHeight()/2;
+		double x = getWidth()/2;
+	    double x0 = getWidth()/2 - FRAME_WIDTH/2;
+		double y = getHeight()/2 - FRAME_HEIGHT/2;
 	    double diff = (FRAME_HEIGHT - LAMP_AMOUNT*2*LAMP_RADIUS)/(LAMP_AMOUNT+1);
+	    double y1 = y + diff;
 	    double dy = diff+2*LAMP_RADIUS;
-	    add(createFilledRect(x,y));
-	    add(createFilledCircle(x,y -dy,LAMP_RADIUS,Color.RED)); 
-	    add(createFilledCircle(x,y,LAMP_RADIUS,Color.YELLOW)); 
-	    add(createFilledCircle(x,y + dy,LAMP_RADIUS,Color.GREEN)); 
+	    add(createFilledRect(x0,y));
+	    add(createFilledCircle(x,y1,LAMP_RADIUS,Color.RED)); 
+	    add(createFilledCircle(x,y1 + dy,LAMP_RADIUS,Color.YELLOW)); 
+	    add(createFilledCircle(x,y1 + 2*dy,LAMP_RADIUS,Color.GREEN)); 
 		
 	}
 	
 	private GRect createFilledRect(double x, double y) {
-		double x0 = x - FRAME_WIDTH/2;
-		double y0 = y - FRAME_HEIGHT/2;
-		GRect frame = new GRect(x0,y0,FRAME_WIDTH, FRAME_HEIGHT);
+	
+		GRect frame = new GRect(x,y,FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setFilled(true);
 		frame.setFillColor(Color.gray);
 	    return frame;
 	}
 	
 	private GOval createFilledCircle(double x, double y, double r, Color color) {
-		GOval circle = new GOval(x-r, y-r, 2*r, 2*r);
+		GOval circle = new GOval(x-r, y, 2*r, 2*r);
 		circle.setFilled(true);
 		circle.setColor(color);
 		return circle;
@@ -41,6 +42,5 @@ public class Ch5Ex6 extends GraphicsProgram{
 	private static final double LAMP_RADIUS = 10;
 	private static final double LAMP_AMOUNT = 3;
 }
-
 
 
